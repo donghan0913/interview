451d6f49faf94f0b0406a2dc2184fc11375c9957
donghan0913 <138421995+donghan0913@users.noreply.github.com>
2025-11-19 11:13:10 +0800

    Update run_top_lp_gate.tcl

--- pipeline_cpu/script/run_top_lp_gate.tcl
+++ pipeline_cpu/script/run_top_lp_gate.tcl
@@ -1,10 +1,10 @@
 source .synopsys_dc.setup
 
 define_design_lib WORK -path ./work
-set PERIOD 6.5
+set PERIOD 6.4
 
 # /* Read Verilog files */
-analyze ./rtl -autoread -format verilog -define {LP_GATE}
+analyze ./rtl -autoread -format verilog -define {MULT_PIP LP_GATE}
 elaborate top
 list_libs
 list_designs
@@ -54,6 +54,24 @@ set_structure false
 compile -gate_clock -map_effort medium
 check_design 
 
+# /* Solve setup violation in GLS, due to ENCLK from cg cell*/
+set enclk [get_pins inst_pipreg_memwb/clk_gate_mem_to_rfile_t3_reg/ENCLK]
+set ep_cells [all_fanout -from $enclk -flat -endpoints_only -only_cells]
+set ck_pins [get_pins -of_object $ep_cells -filter "is_clock_pin==true && (pin_name==CK || pin_name==CP)"]
+set ck_pin_py7 [get_pins inst_pipreg_memwb/py_out_t_reg[7]/CK]
+set ck_pin_py6 [get_pins inst_pipreg_memwb/py_out_t_reg[6]/CK]
+set p_nets [get_nets p]
+#set buf_lib [get_lib_cells slow/CLKBUFX1]
+set buf_lib [get_lib_cells slow/CLKBUFX40]
+
+#insert_buffer -new_cell_names enclk_buf_py7 -new_net_names enclk_n1 $ck_pin_py7 $buf_lib
+#insert_buffer -new_cell_names enclk_buf_py7 -new_net_names enclk_n1 $ck_pin_py6 $buf_lib
+insert_buffer -new_cell_names pbuf -new_net_names p_mid $p_nets $buf_lib
+
+# /* Compile again for setup violation*/
+compile -incremental -map_effort medium
+check_design
+
 # /* Write out report */
 report_area > ./report/top_lp_gate_syn.report
 report_timing -path full -delay max >> ./report/top_lp_gate_syn.report
@@ -67,4 +85,3 @@ write -format verilog -hierarchy -output ./mapped/top_syn.v
 
 # /* Write out SDF */
 write_sdf -version 1.0 -context verilog -load_delay cell ./mapped/top_syn.sdf
-
