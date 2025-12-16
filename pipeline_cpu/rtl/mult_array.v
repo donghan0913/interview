9ded164b8edd8306511d9f2c9592c4993e9ec96d
donghan0913 <138421995+donghan0913@users.noreply.github.com>
2025-12-16 12:21:20 +0800

    Update mult_array.v

--- pipeline_cpu/rtl/mult_array.v
+++ pipeline_cpu/rtl/mult_array.v
@@ -18,6 +18,7 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+//`define MULT_PIP
 
 
 module mult_array #(
@@ -69,6 +70,7 @@ module mult_array #(
     genvar j, k;
     generate
         for(j = 1; j < WIDTH_D + 1; j = j + 1) begin: array_row
+`ifdef MULT_PIP        
             if (j == IDX_PIP) begin: array_row_pip
                 for(k = 0; k < WIDTH_D; k = k + 1) begin: array_col_pip
                     assign {array_carry_pip[k], array_sum_pip[k]} = (a[k] & b[j-1]) + array_sum[j-1][k+1] + array_carry[j-1][k];
@@ -84,11 +86,17 @@ module mult_array #(
                     assign {array_carry[j][k], array_sum[j][k]} = (a[k] & b[j-1]) + array_sum[j-1][k+1] + array_carry[j-1][k];
                 end
             end
+`else
+            for(k = 0; k < WIDTH_D; k = k + 1) begin: array_col
+                assign {array_carry[j][k], array_sum[j][k]} = (a[k] & b[j-1]) + array_sum[j-1][k+1] + array_carry[j-1][k];
+            end
+`endif
         end
     endgenerate
 
     // -------------------------------------------------------------------------------------------------------------------------------------------------------------
     // pipeline
+`ifdef MULT_PIP
     always @(posedge clk) begin
         if (~rst_n) array_sum_pip_t <= 0;
         else array_sum_pip_t <= array_sum_pip;
@@ -108,13 +116,16 @@ module mult_array #(
         if (~rst_n) b_t <= 0;
         else b_t <= b;
     end
-    
+`endif
+
     // -------------------------------------------------------------------------------------------------------------------------------------------------------------
     // CPA and product output
     wire    [WIDTH_P-1:0]           p;                      // 64-bit product
     wire    [IDX_PIP-1:0]           p_lowpip;               // product pipeline needed
     reg     [IDX_PIP-1:0]           p_lowpip_t;             // product after pipeline
     genvar i_p;
+
+`ifdef MULT_PIP
     generate
         for(i_p = 0; i_p < IDX_PIP; i_p = i_p + 1) begin: product_lowpip
             if (i_p == IDX_PIP - 1) begin: product_lowpip_next
@@ -138,6 +149,24 @@ module mult_array #(
     assign p[IDX_PIP-1:0] = p_lowpip_t;
 
     assign p_out = p[WIDTH_D-1:0];
+`else
+    reg     [WIDTH_D-1:0]           p_reg;                  // 32-bit product
+
+    assign p[WIDTH_P-1:WIDTH_D] = array_sum[WIDTH_D][WIDTH_D-1:1] + array_carry[WIDTH_D][WIDTH_D-2:0];
+
+    generate
+        for(i_p = 0; i_p < WIDTH_D; i_p = i_p + 1) begin: product_low
+            assign p[i_p] = array_sum[i_p+1][0];
+        end
+    endgenerate
+    
+    always @(posedge clk) begin
+        if (~rst_n) p_reg <= 0;
+        else p_reg <= p[WIDTH_D-1:0];
+    end
+    
+    assign p_out = p_reg;
+`endif
 
 
 endmodule
